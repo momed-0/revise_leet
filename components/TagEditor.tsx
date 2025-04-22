fc3ecbc19a9d8f9de55f28388ea8cd5296bed4db
momed-0 <muhammedme05@gmail.com>
2025-04-22 00:11:32 +0000

    Removed redundant import in TagEditor.tsx

--- components/TagEditor.tsx
+++ components/TagEditor.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState} from "react";
 import { Badge } from "@/components/ui/badge";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
